ed625575afc417388ee3d1583ac20a0fc35bd04e
Vivek Vijayan <vivek.vijayan01@estudiant.upf.edu>
2024-11-20 13:33:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,23 +8,26 @@ load_dotenv()
 
 app = Flask(__name__)
 
+# Connect to MongoDB
 uri = f"mongodb+srv://{os.getenv('USER')}:{os.getenv('PASSWORD')}@cluster0.dbm54.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(uri, server_api=ServerApi('1'))
 
+# Connect to the database
 db = client["jnd-survey"]
 collection = db["responses"]
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
+        # Get the responses from the form and update the database
         responses = [request.form.get(f"sample-{i}") for i in range(10)]
         responses = [1 if response == "yes" else 0 for response in responses]
-
         collection.insert_one({"responses": responses})
 
         return str(responses)
 
     else:
+        # Render the index page
         samples = [f"audio/sample-{i}.mp3" for i in range(10)]
         return render_template("index.html", samples=samples, enumerate=enumerate)
 
