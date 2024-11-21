ec1e4a99771ad97dd5e076f9c210a8f0b1985f08
Vivek Vijayan <vivek.vijayan01@estudiant.upf.edu>
2024-11-21 13:27:04 +0100

    added samples

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, request
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
+import random
 import os
+import json
 
 load_dotenv()
 
@@ -16,19 +18,25 @@ client = MongoClient(uri, server_api=ServerApi('1'))
 db = client["jnd-survey"]
 collection = db["responses"]
 
+# Select 10 random audio samples and get their info
+samples = os.listdir("static/audio")
+samples = random.sample(samples, 10)
+info = json.load(open("static/info.json"))
+info = {sample: info[sample] for sample in samples}
+
+print(info.values())
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         # Get the responses from the form and update the database
         responses = [request.form.get(f"sample-{i}") for i in range(10)]
         responses = [1 if response == "yes" else 0 for response in responses]
-        collection.insert_one({"responses": responses})
+        collection.insert_one({"samples": samples, "responses": responses})
 
-        return str(responses)
+        return "Thank you for participating in the survey!"
 
     else:
-        # Render the index page
-        samples = [f"audio/sample-{i}.mp3" for i in range(10)]
         return render_template("index.html", samples=samples, enumerate=enumerate)
 
 if __name__ == "__main__":
